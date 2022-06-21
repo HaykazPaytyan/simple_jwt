dae62398ed2ca82559743133b14d8ba40e51a438
HaykazPaytyan <laguna41!>
2022-06-21 10:46:45 +0400

    Delete all from repository

--- src/main/java/org/energize/interfaces/AuthDAO.java
+++
@@ -1,8 +0,0 @@
-package org.energize.interfaces;
-
-import org.energize.domain.Credential;
-import org.energize.domain.User;
-
-public interface AuthDAO {
-    public User attempt(Credential credential);
-}
