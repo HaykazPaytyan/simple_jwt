5615bba976815fd3652b2174dd8723c66a503746
HaykazPaytyan <laguna41!>
2022-08-30 15:48:55 +0400

    Changes 30/08/2022 15:48

--- src/main/java/org/energize/filter/LoginFilter.java
+++ src/main/java/org/energize/filter/LoginFilter.java
@@ -7,6 +7,9 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName = "LoginFilter", urlPatterns = "/login")
@@ -14,6 +17,17 @@ public class LoginFilter extends HttpFilter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpSession session = request.getSession();
+        String token = (String) session.getAttribute("token");
+
+        if (token != null){
+            response.sendRedirect("/profile");
+        }
+
         super.doFilter(req, res, chain);
     }
 }
--- src/main/java/org/energize/filter/RegisterFilter.java
+++ src/main/java/org/energize/filter/RegisterFilter.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName = "RegisterFilter", urlPatterns = "/register")
@@ -13,6 +16,17 @@ public class RegisterFilter extends HttpFilter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        HttpSession session = request.getSession();
+        String token = (String) session.getAttribute("token");
+
+        if (token != null){
+            response.sendRedirect("/profile");
+        }
+
         super.doFilter(req, res, chain);
     }
 }
