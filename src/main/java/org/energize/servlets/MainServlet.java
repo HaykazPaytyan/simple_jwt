fe6999863a3c40303ffdc326fe6003a756e4438d
HaykazPaytyan <laguna41!>
2022-09-01 15:40:27 +0400

    Changes 1/09/2022 15:40

--- src/main/java/org/energize/servlets/MainServlet.java
+++ src/main/java/org/energize/servlets/MainServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(value = "/auth")
-public class AuthServlet extends HttpServlet {
+public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
